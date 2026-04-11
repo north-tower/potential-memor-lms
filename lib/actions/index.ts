29430ad81d6b0280944140b0fe1438b0e293e90d
north-tower <muneshmyke@gmail.com>
2026-04-11 14:05:26 +0300

    course detail

---
+++ lib/actions/index.ts
@@ -0,0 +1,5 @@
+// Barrel export for all server actions
+export { toggleLessonCompletion } from "./lessons";
+export { toggleCourseCompletion } from "./courses";
+export { getMuxSignedToken, getMuxSignedTokens } from "./mux";
+export { uploadImage, deleteImage } from "./images";
