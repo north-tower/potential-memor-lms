50f507cf9ba3c4f5efcaa01524048829bef2bc4f
north-tower <muneshmyke@gmail.com>
2026-04-11 11:41:32 +0300

    sanity phase 1

---
+++ sanity/lib/queries.ts
@@ -0,0 +1,21 @@
+import { defineQuery } from "next-sanity";
+
+export const FEATURED_COURSES_QUERY = defineQuery(`*[
+  _type == "course"
+  && featured == true
+] | order(_createdAt desc)[0...6] {
+  _id,
+  title,
+  slug,
+  description,
+  tier,
+  featured,
+  thumbnail {
+    asset-> {
+      _id,
+      url
+    }
+  },
+  "moduleCount": count(modules),
+  "lessonCount": count(modules[]->lessons[])
+}`);
