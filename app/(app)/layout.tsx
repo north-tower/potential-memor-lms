631a915c589bb6ac38475ef6ff216439e32b5af2
north-tower <muneshmyke@gmail.com>
2026-04-20 14:54:35 +0300

    ai v2

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import { TutorWidget } from "@/components/tutor"
 import { SanityLive } from "@/sanity/lib/live"
 import { ClerkProvider } from "@clerk/nextjs"
 
@@ -7,6 +8,7 @@ function AppLayout({ children }: Readonly <{ children: React.ReactNode }  >) {
     <ClerkProvider>
     <div>{children}</div>
     <SanityLive />
+    <TutorWidget />
     </ClerkProvider>
   )
 }
